9e0965f8770cc5280554917873f7d6182dfdc5c4
Abdel Latif Hamdan <71897612+Aboudi31@users.noreply.github.com>
2024-04-30 11:03:47 +0200

    location UI display

--- app/src/main/java/com/example/trackerapp/MainScreen.kt
+++ app/src/main/java/com/example/trackerapp/MainScreen.kt
@@ -2,43 +2,41 @@ package com.example.trackerapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material3.Text
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = Brush.linearGradient(
-                    colors = listOf(
-                        Color(0xFFFF1C31),
-                        Color(0xFFD71729)
-                    )
-                )
-            ),
-    ) {
+
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(20.dp),
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color(0xFFFF1C31),
+                            Color(0xFFD71729)
+                        )
+                    )
+                )
+                .padding(vertical = 10.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
             Image(
                 modifier = Modifier.size(150.dp, 150.dp),
@@ -49,11 +47,15 @@ fun MainScreen() {
                 color = Color.White,
                 fontSize = 26.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(16.dp),
                 text = "Linked devices"
             )
+            Text(
+                color = Color.White,
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            text = "Longitude : ${1} | Latitude : ${2}"
+            )
         }
-    }
 }
 
 @Preview(showBackground = true)
